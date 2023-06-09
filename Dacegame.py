b5d9546c9eec406312b3e13c37ca89638ceee33b
s1290124 <>
2023-06-09 11:05:53 +0900

    Won/lost message added

--- Dacegame.py
+++ Dacegame.py
@@ -10,3 +10,8 @@ total_value = roll1 + roll2
 print("Die 1: ",roll1)
 print("Die 2: ",roll2)
 print("Total value: ",total_value)
+
+if total_value > 7:
+    print("You won!")
+else:
+    print("You lost")
