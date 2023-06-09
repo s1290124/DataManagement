8e48c7ab3ae406f05f63a686cb30c4f043b8d811
s1290124 <>
2023-06-09 10:21:36 +0900

    Dice game: initial revision

---
+++ Dacegame.py
@@ -0,0 +1,12 @@
+import random
+
+print("Rolling dace...")
+
+roll1 = random.randint(1,6)
+roll2 = random.randint(1,6)
+
+total_value = roll1 + roll2
+
+print("Die 1: ",roll1)
+print("Die 2: ",roll2)
+print("Total value: ",total_value)
